6f6a40c90f76045c3a60f077c121954721335658
raghulrajg <raghulrajatmega328@gmail.com>
2024-05-31 20:31:11 +0530

    minimal change

--- miniProject.cpp
+++ miniProject.cpp
@@ -3,11 +3,11 @@
 WiFiClient client;
 WiFiClient httpclient;
 HTTPClient http;
-PubSubClient miniProject::mqttclient(client);
+miniProject *instance = new miniProject;
 
 void update_started()
 {
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
 #ifdef WM_DEBUG_LEVEL
   instance->DEBUG_WM(F("CALLBACK:  HTTP update process started"));
 #endif
@@ -15,7 +15,7 @@ void update_started()
 
 void update_finished()
 {
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
 #ifdef WM_DEBUG_LEVEL
   instance->DEBUG_WM(F("CALLBACK:  HTTP update process finished"));
 #endif
@@ -24,7 +24,7 @@ void update_finished()
 
 void update_progress(int Cur, int Total)
 {
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
   digitalWrite(2, HIGH);
 #ifdef WM_DEBUG_LEVEL
   instance->DEBUG_WM(F("CALLBACK:  HTTP update process and bytes"), String(Cur) + " " + String(Total));
@@ -34,7 +34,7 @@ void update_progress(int Cur, int Total)
 
 void update_error(int err)
 {
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
 #ifdef WM_DEBUG_LEVEL
   instance->DEBUG_WM(F("CALLBACK:  HTTP update fatal error code "), err);
 #endif
@@ -42,7 +42,7 @@ void update_error(int err)
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
   char msg[length + 1];
   memcpy(msg, payload, length);
   msg[length] = '\0';
@@ -73,8 +73,8 @@ void miniProject::mqtt(int cur = 0, int total = 0)
   String topic = "state/" + _User;
   if (mqttclient.publish(topic.c_str(), out))
   {
-     //Serial.print("message is send! ");
-     //Serial.println(out);
+    // Serial.print("message is send! ");
+    // Serial.println(out);
     digitalWrite(2, LOW);
   }
   else
@@ -114,7 +114,7 @@ void miniProject::wifimanagerConn(char const *apName, char const *apPassword, mi
 void miniProject::OTAUpdate()
 {
   WiFiClient client1;
-  miniProject *instance = new miniProject;
+  // miniProject *instance = new miniProject;
 
   miniProject::mqtt();
 
@@ -267,6 +267,8 @@ const int miniProject::mqtt_port = 2512;
 const String miniProject::mqtt_username = "raghulrajg";
 const String miniProject::mqtt_password = "Gr2_nemam";
 int miniProject::_StatusCode = 0;
+
+PubSubClient miniProject::mqttclient(client);
 
 String miniProject::httpGETRequest(miniProject *instance, const char *serverName)
 {
